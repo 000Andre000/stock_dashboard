846d8fe021e599340dd8d343744b5920d5c7f9a2
000Andre000 <89241490+000Andre000@users.noreply.github.com>
2025-01-23 20:59:19 +0530

    Delete backend directory

--- backend/sms-backend/src/main/java/com/example/sms/repository/StockRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sms.repository;
-
-import com.example.sms.model.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StockRepository extends JpaRepository<Stock, Long> {
-}
